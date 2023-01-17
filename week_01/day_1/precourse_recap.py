42e159825ac27a29427c9aa8a8f772d9503dbd96
Stuart-McCafferty <Stuartj678@gmail.com>
2023-01-17 15:48:41 +0000

    inital commit

---
+++ week_01/day_1/precourse_recap.py
@@ -0,0 +1,16 @@
+#Stuart McCafferty
+
+print("Hello World!")
+
+value1 = 6
+value2 = 4
+
+print(value1)
+print(value2)
+print(value1 + value2)
+result = value1+ value2
+print(result)
+print(5 + 3)
+print(4 / 2)
+print(10 % 3)
+print(10 - 2)
