dc09e8657d52784bd01d877799e0b61bb9c653a0
Stuart-McCafferty <Stuartj678@gmail.com>
2023-01-17 20:03:05 +0000

    completed lists

---
+++ week_01/day_2/lists.py
@@ -0,0 +1,28 @@
+# Stuart McCafferty
+
+fruits = ['apple', 'banana', 'pineapple', 'grape']
+
+
+print(fruits)
+print(fruits[0])
+print(fruits[-1])
+print(fruits[3])
+fruits[0] = 'mango'
+print(fruits)
+
+empty_list = []
+empty_list2 = list()
+print(empty_list)
+print(empty_list2)
+
+print(len(fruits))
+fruits.append('pear')
+print(fruits)
+fruits.pop()
+print(fruits)
+
+fruits_and_numbers = ['apple', 1, 'pear', 2]
+print(fruits_and_numbers)
+
+nested_list = [1, 2, 3, [4, 5, 6], 7]
+
