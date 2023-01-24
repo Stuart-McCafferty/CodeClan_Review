d2cb0a0a307de6bf0d021926eaf2f8154e827024
Stuart-McCafferty <Stuartj678@gmail.com>
2023-01-24 20:43:54 +0000

    finished lab

--- week_02/day_1/single_class_lab_start_code/team_class/src/team.py
+++ week_02/day_1/single_class_lab_start_code/team_class/src/team.py
@@ -3,4 +3,18 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
-        
+        self.points = 0
+    
+    def add_player(self, player):
+        self.players.append(player)
+    
+    def has_player(self, player):
+        for person in self.players:
+            if person == player:
+                return True
+        return False
+    
+    def play_game(self, result):
+        if result:
+            self.points += 3
+                    
